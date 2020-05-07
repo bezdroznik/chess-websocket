7cf962dcb8076691e578e74dbda1b6564578c09b
bezdroznik <piojar52@gmail.com>
2020-05-07 22:23:50 +0200

    added websockets

---
+++ src/main/java/pl/bezdroznik/chesswebsocket/websocket/OutputMessage.java
@@ -0,0 +1,17 @@
+package pl.bezdroznik.chesswebsocket.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class OutputMessage {
+
+    private String from;
+    private String text;
+    private String time;
+}
