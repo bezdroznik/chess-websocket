2e327d82a4c2bb7cef7d8a9fa068c58b193c5ba1
bezdroznik <piojar52@gmail.com>
2020-05-07 20:55:27 +0200

    my first commit

---
+++ src/main/java/pl/bezdroznik/chesswebsocket/ChessWebsocketApplication.java
@@ -0,0 +1,13 @@
+package pl.bezdroznik.chesswebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessWebsocketApplication.class, args);
+	}
+
+}
